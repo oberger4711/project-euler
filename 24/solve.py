e80a48c169f802022c3804b1c451c5a7e5a21bae
oberger <ole.k.berger@t-online.de>
2016-01-10 00:10:56 +0100

    Solve problem 24.

--- 24/solve.py
+++ 24/solve.py
@@ -7,13 +7,13 @@
 import math
 
 #digits = [0, 1, 2]
-digits = [0, 1, 2, 3]
-#digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+#digits = [0, 1, 2, 3]
+digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 def nextLexicalPerm(a):
-    for r in reversed(range(0, len(a))):
+    for l in reversed(range(0, len(a))):
         swapped = False
-        for l in reversed(range(0, r)):
+        for r in reversed(range(l, len(a))):
             if a[l] < a[r]:
                 a[l], a[r] = a[r], a[l]
                 a[l + 1:] = sorted(a[l + 1:])
@@ -22,8 +22,12 @@ def nextLexicalPerm(a):
         if swapped:
             break
 
+#LIMIT = math.factorial(len(digits)) - 1
+LIMIT = 1000000 - 1
 print digits
-for i in range(0, math.factorial(len(digits)) - 1):
+for i in range(0, LIMIT):
     nextLexicalPerm(digits)
-    print digits
+    #print digits
+
+print digits
 
