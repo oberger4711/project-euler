7b393e8ecd56ddc9dea491b65359ef98b0b2b9be
oberger <ole.k.berger@t-online.de>
2016-03-03 01:24:19 +0100

    Solve problem 31.

--- 31/solve.py
+++ 31/solve.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python
 
 # https://projecteuler.net/problem=31
-# Simple depth first search.
+# Start with the smallest coin and add each one multiple times.
+# Track how much possibilities are known yet for each value.
+# After each check also add the combination with known value pathes.
+# Check the image for a handwritten example.
 
+wanted = 200
 coins = [1, 2, 5, 10, 20, 50, 100, 200]
 cache = {}
 
-def search(wanted, path = [], maxCoin = min(coins)):
-    res = 0
-    diff = wanted - sum(path)
-    if (diff == 0):
-        res = 1
-    elif (diff > 0):
-        if (diff in cache):
-            res = cache[diff]
-        else:
-            if (coin <= maxCoins):
-                extendedPath = path[:]
-                extendedPath.append(coin)
-                res += search(wanted, extendedPath, coin)
-    return res
+# Initialize cache.
+for i in range(1, wanted + 1):
+    cache[i] = 0
 
-print search(5)
+for coin in coins:
+    # Old cache must not be changed until the new one is completely updated.
+    newCache = cache.copy()
+    for nums in range(1, (wanted / coin) + 1):
+        value = nums * coin
+        newCache[value] += 1
+        for knownValue, knownPathes in cache.items():
+            valueSum = knownValue + value
+            if (valueSum <= wanted):
+                newCache[valueSum] += knownPathes
+            else:
+                break
+    cache = newCache
+
+print cache[wanted]
