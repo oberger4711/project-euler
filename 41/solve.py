6ffdd1b9db7109367d7a9b3549685c8c13b1b861
oberger <ole.k.berger@t-online.de>
2016-03-04 17:53:52 +0100

    Start solving problem 41.

---
+++ 41/solve.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+from math import sqrt
+
+# https://projecteuler.net/problem=41
+
+digits = []
+for i in range(1, 10):
+    digits.append(str(i))
+print "Setup digits", digits
+def isPandigital(nString):
+    for digitStr in digits[:len(nString)]:
+        if (digitStr not in nString):
+            return False
+    return True
+
+def isPrime(n):
+    # Should only go up to sqrt n.
+    for i in xrange(2, int(sqrt(n)) + 1):
+        if (n % i == 0):
+            return False
+    return True
+
+relevant = xrange(2143, 987654322)
+for n in reversed(relevant):
+    if (isPandigital(str(n)) and isPrime(n)):
+        print n
+
+print "Done."
