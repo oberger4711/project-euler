7b10de29a6682731389714561e789f3c64b7aaea
Ole Berger <ole.k.berger@t-online.de>
2018-05-12 14:58:56 +0200

    Speed up problem 49 a bit.

--- 49/solve.py
+++ 49/solve.py
@@ -5,16 +5,11 @@ import utils
 # https://projecteuler.net/problem=49
 # Brute Force.
 
-primes = {}
-for i in range(1000, 10000):
-    if utils.isPrime(i):
-        primes[i] = True
-
 for i_0 in range(1000, 10000):
-    if i_0 in primes:
+    if utils.isPrime(i_0):
         for inc in range(1, (10000 - i_0) // 2):
             i_1 = i_0 + inc
             i_2 = i_1 + inc
             if utils.arePermutationsShort(i_0, i_1) and utils.arePermutationsShort(i_1, i_2):
-                if i_1 in primes and i_2 in primes:
+                if utils.isPrime(i_1) and utils.isPrime(i_2):
                     print("{}, {}, {} (concatenated: {})".format(i_0, i_1, i_2, "".join((str(i_0), str(i_1), str(i_2)))))
